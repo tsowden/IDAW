8a6f5a538ec8146b773f1c3efc7696a9f094ca6f
tsowden <tsowden.sup@gmail.com>
2023-10-10 17:24:36 +0200

    TP2 - complet (10/10/23)

--- TP2-PHP/v3/template_footer.php
+++ TP2-PHP/v3/template_footer.php
@@ -5,7 +5,7 @@
     <title>Tom Sowden</title>
     <link rel="stylesheet" href="style1.css" type="text/css" media="screen" title="default" charset="utf-8" />
 
-    <footer>Disponible par mail ou par téléphone de 8h à 18h. Voir ma page <span class="text-bold">Contact</span> pour davantage de renseignements.</footer>
+    <footer>Disponible par mail ou par téléphone de 8h à 18h. Voir ma page <span class="text-bold"><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley">Contact</a></span> pour davantage de renseignements.</footer>
 </head>
 
 <body>
