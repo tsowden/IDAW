b9bfa6fafa26a0d45fe257efa53d19b473fea9f2
tsowden <tsowden.sup@gmail.com>
2023-10-11 11:34:28 +0200

    TP 3 fin (11/10/23)

--- TP3/logout.php
+++ TP3/logout.php
@@ -1,7 +1,15 @@
-<?php
-session_start(); // Démarre la session
-session_destroy(); // Détruit la session
+<!DOCTYPE html>
+<html>
 
-// Redirigez l'utilisateur vers une page de votre choix (par exemple, la page d'accueil)
-header("Location: index.php");
-?>
+<head>
+</head>
+
+<body>
+    <?php
+    session_start(); // Démarre la session
+    session_destroy(); // Détruit la session
+
+    // Redirigez l'utilisateur vers une page de votre choix (par exemple, la page d'accueil)
+    header("Location: index.php");
+    ?>
+</body>
